bc9be5cedebe09b1d20790b2478882410375a065
Martin Gilles <martin.gilles.42@gmail.com>
2019-10-13 18:45:19 +0200

    add testcase

--- epsilon_test.go
+++ epsilon_test.go
@@ -12,6 +12,7 @@ func TestEpsilon(t *testing.T) {
 	}{
 		{"t1", 1e-4, 567.67, 567.671, false},
 		{"t2", 1e-4, 567.67123, 567.67124, true},
+		{"t3", 10, 11, 22, false},
 	}
 
 	for _, test := range tests {
